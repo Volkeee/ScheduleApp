b35c88e4c828bd63204bcae7785d1bfe6735e4b2
Volkeee <volkeee1@gmail.com>
2017-12-02 13:17:39 +0200

    BaseModel class created to be a parent class for all the models

---
+++ app/src/main/java/com/ifntuog/volkeee/schedule/model/BaseModel.java
@@ -0,0 +1,34 @@
+package com.ifntuog.volkeee.schedule.model;
+
+/**
+ * Created by volkeee on 12/1/17.
+ */
+
+public class BaseModel {
+    public Integer id;
+    public String name;
+
+    public BaseModel() {
+    }
+
+    public BaseModel(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
