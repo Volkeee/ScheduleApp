823e03f0baff5f0670ec66bcae8f77181a3d7e94
Volkeee <volkeee1@gmail.com>
2017-12-05 01:59:36 +0200

    Initial creation of DatesManager to manipulate dates and days within the app

---
+++ app/src/main/java/com/ifntuog/volkeee/schedule/tools/manager/DatesManager.java
@@ -0,0 +1,49 @@
+package com.ifntuog.volkeee.schedule.tools.manager;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by volkeee on 12/2/17.
+ */
+
+public class DatesManager {
+    private Date startingDate;
+
+    public DatesManager() {
+
+    }
+
+    private static List<Date> getDates(String dateString1, String dateString2) {
+        ArrayList<Date> dates = new ArrayList<Date>();
+        DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date date1 = null;
+        Date date2 = null;
+
+        try {
+            date1 = df1.parse(dateString1);
+            date2 = df1.parse(dateString2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+
+
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+
+        while (!cal1.after(cal2)) {
+            dates.add(cal1.getTime());
+            cal1.add(Calendar.DATE, 1);
+        }
+        return dates;
+    }
+}
