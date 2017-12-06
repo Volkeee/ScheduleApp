03cd5718613f2c616c4b501ce96bbe78512b1829
Volkeee <volkeee1@gmail.com>
2017-12-07 00:50:01 +0200

    OnItemClickListener interface has been created to handle onClick events for recyclerViews in the application

---
+++ app/src/main/java/com/ifntuog/volkeee/schedule/adapter/OnItemClickListener.java
@@ -0,0 +1,11 @@
+package com.ifntuog.volkeee.schedule.adapter;
+
+import com.ifntuog.volkeee.schedule.model.Lesson;
+
+/**
+ * Created by volkeee on 12/6/17.
+ */
+
+public interface OnItemClickListener {
+    void onItemClick(Lesson lesson);
+}
